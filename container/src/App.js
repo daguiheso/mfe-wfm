532046243accf3038ac5e65941e216a6b38efe4b
Daniel H <danielhernandez16@hotmail.com>
2022-07-21 13:30:19 -0500

    feat(container): use mount from remote Marketing

--- container/src/App.js
+++ container/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import MarketingApp from './components/MarketingApp'
 
 const App = () => {
   return (
-    <h1>Hi There App</h1>
+    <>
+      <h1>Hi there!</h1>
+      <MarketingApp />
+    </>
   )
 }
 
