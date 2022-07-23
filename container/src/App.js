69bc4b9c2c66c004cb317af949a59e9fab8bfcdb
Daniel H <danielhernandez16@hotmail.com>
2022-07-23 13:39:56 -0500

    feat(container): change text

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp'
 const App = () => {
   return (
     <>
-      <h1>Hi there!</h1>
+      <h1>Microfronts</h1>
       <MarketingApp />
     </>
   )
