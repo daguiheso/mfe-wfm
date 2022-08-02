34528d21220f2c2fb43905570bcafbdf72284f81
Daniel H <danielhernandez16@hotmail.com>
2022-08-01 20:37:45 -0500

    feat(container): implement memoryHistory

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 import './scss/index.scss'
 
 export default () => {
   return (
-    <BrowserRouter>
+    <MemoryRouter>
       <Routes>
         <Route exact path="pricing" element={<Pricing/>} />
         <Route path="/" element={<Landing/>} />
       </Routes>
-    </BrowserRouter>
+    </MemoryRouter>
   )
 }
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,3 +1,5 @@
+// use var global here
+
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
